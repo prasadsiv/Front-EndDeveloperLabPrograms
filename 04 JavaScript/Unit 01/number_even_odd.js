994f1662bdc06a04da6065dff87db2653cfd73bc
Sivaprasad.G <sivaprasad.g@live.com>
2023-11-04 09:57:20 +0530

    day1_finished

---
+++ 04 JavaScript/Unit 01/number_even_odd.js
@@ -0,0 +1,15 @@
+// The first variable, named num1,  should be assigned a number value of 2.
+var num1 = 2;
+// The second variable, named num2, should be assigned a number value of 5.
+var num2 = 5;
+// The third variable, named test1, should be assigned the calculation of num1 % 2. Note: executing this code will return a number.
+var test1 = num1 % 2;
+// The fourth variable, named test2, should be assigned the calculation of num2 % 2. Note: executing this code will also return a number.
+var test2 = num2 % 2;
+// The fifth variable, named result1, should be assigned the result of comparing if the number stored in the test1 variable is not equal to 0, in other words, this: test1 == 0.
+var result1 = test1 == 0;
+// The sixth variable, named result2, should be assigned the result of comparing if the number stored in the test2 variable is not equal to 0, in other words, test2 == 0.
+var result2 = test2 == 0;
+
+console.log("Is ", num1, "an even number ?", result1);
+console.log("Is ", num2, "an even number ?", result2);
